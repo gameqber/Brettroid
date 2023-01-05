c879c484ef1717d367e380629e60b5c4d2cb45c0
nerevolution <15821063+nerevolution@users.noreply.github.com>
2023-01-04 23:27:53 -0500

    Add raycasts to Zoomers
     - Debug log shows what is being hit, or no collision detected

--- Assets/Zoomer.cs
+++ Assets/Zoomer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Zoomer : MonoBehaviour
 {
     public float speed = 2.0f;
+    private bool messagePrinted = false;
+    private int counter = 0;
     [SerializeField] private int moveRight = 1; // When -1, it will move left
     // private int moveSpriteDirection = 0; // 0 = right, 1 = up, 2 = left, 3 = down
     // Start is called before the first frame update
@@ -15,10 +17,25 @@ public class Zoomer : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        Debug.DrawRay(transform.position, transform.right * 2, Color.green);
     }
     void FixedUpdate()
     {
+        Debug.DrawRay(transform.position - new Vector3(0.5f, 0.5f, 0), transform.up * -0.5f, Color.green);
+        Debug.DrawRay(transform.position + new Vector3(0.5f, -0.5f, 0), new Vector3(0.5f, -0.5f, 0), Color.yellow);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position - new Vector3(0.55f, 0.55f, 0), transform.up * -0.5f, 5f);
+        if (hit.collider == null)
+        {
+            //Did not hit something
+            Debug.Log("No more surface!");
+            messagePrinted = true;
+        }
+        else if(messagePrinted == false && counter % 60 == 0)
+        {
+            //Did not hit something
+            Debug.Log("Hitting: " + hit.collider.tag);
+            // messagePrinted = true;
+        }
+        counter++;
         moveSprite();
         
     }
