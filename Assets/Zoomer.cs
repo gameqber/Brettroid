dac90dbf63b6bc8eb8ed8750f46f920cac725d88
GameQber <15821063+gameqber@users.noreply.github.com>
2023-01-11 19:59:32 -0500

    Shrink Zoomer Box Collider
    - Fixes erroneous detection of collisions with walls and causing rotation

--- Assets/Zoomer.cs
+++ Assets/Zoomer.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Zoomer : MonoBehaviour
 {
     public float speed = 2.0f;
-    private bool messagePrinted = false;
+    public bool enableTracking = false;
+    private GameObject downRay, diagRay, wallCollision;
     private int counter = 0;
+    private bool initialRotation = true;
     [SerializeField] private int moveRight = 1; // When -1, it will move left
     // private int moveSpriteDirection = 0; // 0 = right, 1 = up, 2 = left, 3 = down
     // Start is called before the first frame update
     void Start()
     {
-
+        downRay = transform.GetChild(0).gameObject;
+        diagRay = transform.GetChild(1).gameObject;
+        wallCollision = transform.GetChild(2).gameObject;
     }
 
     // Update is called once per frame
@@ -20,46 +24,64 @@ public class Zoomer : MonoBehaviour
     }
     void FixedUpdate()
     {
-        Debug.DrawRay(transform.position - new Vector3(0.5f, 0.5f, 0), transform.up * -0.5f, Color.green);
-        Debug.DrawRay(transform.position + new Vector3(0.5f, -0.5f, 0), new Vector3(0.5f, -0.5f, 0), Color.yellow);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position - new Vector3(0.55f, 0.55f, 0), transform.up * -0.5f, 5f);
-        if (hit.collider == null)
-        {
-            //Did not hit something
-            Debug.Log("No more surface!");
-            messagePrinted = true;
+        // RaycastHit2D downhit = Physics2D.Raycast(transform.position - new Vector3(0.55f, 0.55f, 0), transform.up * -0.5f, 0.5f);
+        RaycastHit2D downhit = Physics2D.Raycast(downRay.transform.position, transform.up * -0.5f, 0.5f);
+        if(enableTracking){
+            Debug.DrawRay(downRay.transform.position, transform.up * -0.5f, Color.green);
+            Debug.DrawRay(diagRay.transform.position, transform.up * -0.5f + transform.right * 0.5f, Color.yellow);
+            /* if (downhit.collider == null && counter % 60 == 0)
+            {
+                //Did not hit something
+                Debug.Log("No more surface!");
+            }
+            else if(counter % 60 == 0)
+            {
+                //Did not hit something
+                Debug.Log("Hitting: " + downhit.collider.tag);
+            }
+            counter++; */
         }
-        else if(messagePrinted == false && counter % 60 == 0)
-        {
-            //Did not hit something
-            Debug.Log("Hitting: " + hit.collider.tag);
-            // messagePrinted = true;
+
+        // If nothing is below, detect for floor Zoomer is moving onto. If there's no floor there either, rotate.
+        if(downhit.collider == null) {
+            if(initialRotation){
+                if(enableTracking){
+                    Debug.Log("Initial rotation");
+                }
+                
+                transform.Rotate(new Vector3(0, 0, -90));
+                initialRotation = false;
+            }
+            RaycastHit2D diaghit = Physics2D.Raycast(diagRay.transform.position, transform.up * -0.5f + transform.right * 0.5f, 0.5f);
+            if(diaghit.collider == null){
+                if(enableTracking)
+                    Debug.Log("Rotating clockwise");
+                transform.Rotate(new Vector3(0, 0, -90));
+            }
         }
-        counter++;
+        else{
+            initialRotation = true;
+        }
         moveSprite();
         
     }
 
+    void OnCollisionEnter2D(Collision2D collision){
+        Collider2D collider = collision.collider;
+        if(enableTracking)
+            Debug.Log("Collision detected between " + this.name + " and " + collision.gameObject.name);
+        if(collision.gameObject.tag == "Floor"){
+            Vector3 contactPoint = collision.contacts[0].point;
+            Vector3 center = collider.bounds.center;
+
+            transform.Rotate(new Vector3(0, 0, 90));
+        }
+        /* if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Player") {
+            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+        } */
+    }
+
     void moveSprite(){
-        transform.position += (Vector3.right * speed * moveRight) * Time.deltaTime;
-        /*
-        switch (moveSpriteDirection){
-                case 0:
-                    // Increment X
-                    break;
-                case 1:
-                    // Increment Y
-                    break;
-                case 2:
-                    // Decrement X
-                    break;
-                case 3:
-                    // Decrement Y
-                    break;
-                default:
-                    // Don't move
-                    break;
-            }
-        */
+        transform.position += (transform.rotation * Vector3.right * speed * moveRight) * Time.deltaTime;
     }
 }
